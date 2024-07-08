82867d8ea513b26128e0ef4dc309a8ed7c3e3e02
Aladi TIMERA <aladi.timera@eren.lan>
2024-07-08 17:35:44 +0200

    first documentation

--- documentation.js
+++ documentation.js
@@ -1,6 +1,10 @@
 /*
+le lien du video de la documentation
 
-On va déployer ce code via github
+https://www.youtube.com/watch?v=6GD7qTDWMKY&list=PLA9C2nzMpRuHIORCkWUmEeFsKhf9dt34x
+
+
+On va déployer ce code via github et netlify
 
 
 
