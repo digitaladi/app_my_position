e16b3979d60d52074601d234f3f1183f8eb17c8b
Aladi TIMERA <aladi.timera@eren.lan>
2024-07-17 18:03:04 +0200

    avant conflit sur main

--- documentation.js
+++ documentation.js
@@ -14,3 +14,9 @@ On va déployer ce code via github et netlify
 
 /* Pour les bonnes pratiques , on va intégrer la notion de revue de code . 
 Pour cela, on va créer une branche , faire des modifications, les envoyer sur le dépot distant, puis créer une pull request pour demander une revue de code */
+
+
+
+
+
+/* j'ai modifié sur le meme fichier et sur la meme ligne dans la branche main donc bonjour les conflits avec dev */
